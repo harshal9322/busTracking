faea2af519dfd2ffa5083e9a8543173abd2b06af
harshal9322 <harsh932206@gmail.com>
2025-12-15 08:57:47 +0530

    remove rout and instead 'from' and 'to'

--- models/Bus.js
+++ models/Bus.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const busSchema = new mongoose.Schema({
   busNumber: { type: String, required: true },
   driverName: { type: String, required: true },
-  route: {type:String},
+  from: {type:String, required:true},
+  to:{type:String, required:true},
   currentLocation:{
     lat:{type:Number, default:0},
     lng:{type:Number, default:0}
