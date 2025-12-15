3009adfb950dc4472c8b1e54c6ff24d12e7606fb
harshal9322 <harsh932206@gmail.com>
2025-12-15 15:59:24 +0530

    add busses rotute

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { userRegister , userLogin} from "../controllers/userController.js";
+import { userRegister , userLogin, findBus} from "../controllers/userController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/register",userRegister);
 
 // Login
 router.post("/login", userLogin);
+// find Buses
+router.post("/login/buses", authMiddleware, findBus);
 
 export default router;
